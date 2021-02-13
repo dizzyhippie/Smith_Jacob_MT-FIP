3d221c5d9c00ee0d2ad930aa217fc77a1b5ae7e4
Jacob Smith <77408757+dizzyhippie@users.noreply.github.com>
2021-02-13 13:43:22 -0500

    First JS changes
    Made first changes on dev branch and made the icons on products page interactive.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,29 @@
 console.log("javascript is linked!");
 
+(() =>{
+
+"use strict";
+
+let hopsIcon = document.querySelector('#hops');
+let barleyIcon = document.querySelector('#barley');
+let pintIcon = document.querySelector('#pint');
+let kegIcon = document.querySelector('#keg');
+
+
+
+function clicked(){
+	console.log('You have selected an Icon');
+	console.log('You picked', this.id);
+}
+
+
+
+hopsIcon.addEventListener("click", clicked);
+barleyIcon.addEventListener("click", clicked);
+pintIcon.addEventListener("click", clicked);
+kegIcon.addEventListener("click", clicked);
+
+
+})();
+
+
